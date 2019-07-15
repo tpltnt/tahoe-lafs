4c6c0945f10ebf275d5649cad2fd4e301460747d
tpltnt <1172976+tpltnt@users.noreply.github.com>
2019-07-15 16:52:26 +0200

    added (regression) tests for format_delta()

--- src/allmydata/test/test_util_time_format.py
+++ src/allmydata/test/test_util_time_format.py
@@ -2,7 +2,7 @@
 Tests for the time string formatting functions in util/time_format.py
 """
 from twisted.trial import unittest
-from allmydata.util.time_format import parse_duration
+from allmydata.util.time_format import format_delta, parse_duration
 
 class ParseDuration(unittest.TestCase):
     """
@@ -42,3 +42,27 @@ class ParseDuration(unittest.TestCase):
         """
         self.failUnlessRaises(ValueError, parse_duration, "lalala")
         self.failUnlessRaises(ValueError, parse_duration, "5")
+
+
+class FormatDelta(unittest.TestCase):
+    """
+    Tests for the format_delta() function.
+    """
+
+    def test_positive_examples(self):
+        """
+        minimal positive tests
+        """
+        self.assertEqual("5s", format_delta(6, 11))
+        self.assertEqual("1m 10s", format_delta(100, 170))
+        self.assertEqual("1h 3m 35s", format_delta(1000, 4815))
+        self.assertEqual("1d 0h 32m 28s", format_delta(10000, 98348))
+
+
+    def test_errors(self):
+        """
+        test errors
+        """
+        self.assertEqual("N/A", format_delta(None, 11))
+        self.assertEqual("N/A", format_delta(7, None))
+        self.assertEqual("-", format_delta(7, 1))
